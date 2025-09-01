0e6acf8f98f1faff79d04c1a613c2039a2287f77
Nikita KoSH <150700636+Inseliq@users.noreply.github.com>
2025-09-01 20:12:06 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeSparkNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

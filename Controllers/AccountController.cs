f25d61c76cbc5b600e1b5c2f0a65072bc6b9334f
Nikita KoSH <150700636+Inseliq@users.noreply.github.com>
2025-09-03 18:29:13 +0300

    register page html

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using CodeSparkNET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSparkNET.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
